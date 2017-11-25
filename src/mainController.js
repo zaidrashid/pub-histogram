95466080337f5e8ab09cd0066e849ab68b00e841
zaidrashid <zed182@gmail.com>
2017-11-25 12:25:13 +0800

    Revert "Init commit"
    This reverts commit bf3154fc6ca483eb3bb5f5c85b0e843e781ca354.

--- src/mainController.js
+++
@@ -1,8 +0,0 @@
-(function($angular) {
-    $angular.module('pubHistogram', []);
-    var app = $angular.module('pubHistogram');
-
-    app.controller('mainController', function($scope) {
-        $scope.message = 'Loaded';
-    });
-})(window.angular);
